a62ba869c235ee43bbaaf59079de30f03019ef89
Dmitry Fedorov <d.fedorov@tabit.pro>
2021-01-13 19:21:48 +0300

    winusb: identify usb device using controller id

--- helpers/qusb.sh
+++ helpers/qusb.sh
@@ -6,7 +6,8 @@ if [ "$ACTION" == "bind" ]; then
 	SPEED=`sed -n '1p;' /sys/$DEVPATH/speed`
 	if [ $SPEED -gt 480 ]; then BUS="xhci.0"; else BUS="ehci.0"; fi;
 	echo "$SPEED $BUS $DEVPATH" >> /tmp/qusb.log
-        echo '{ "execute": "device_add", "arguments": { "driver": "usb-host", "id": "busid-'"`busybox basename $MDEV`"'", "bus": "'"$BUS"'", "guest-reset": "False" } }' > /tmp/qmp/qemu.in
+	BUSID="busid-$BUS-`busybox basename $MDEV`"
+        echo '{ "execute": "device_add", "arguments": { "driver": "usb-host", "id": "'"$BUSID"'", "bus": "'"$BUS"'", "guest-reset": "False" } }' > /tmp/qmp/qemu.in
 elif [ "$ACTION" == "unbind" ]; then
 	echo "done" >> /tmp/qusb.log
 	echo '{ "execute": "device_del", "arguments": { "id": "busid-'"`busybox basename $MDEV`"'" } }' > /tmp/qmp/qemu.in
