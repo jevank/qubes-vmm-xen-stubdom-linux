3ef62947069a94ebbf5edf8b02e0fa593724a7d7
Dmitry Fedorov <d.fedorov@tabit.pro>
2021-01-14 14:36:38 +0300

    winusb: use /dev/mdev.log

--- helpers/qusb.sh
+++ helpers/qusb.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 
-echo -n "$ACTION $MDEV " >> /tmp/qusb.log
-
 if [ "$ACTION" == "bind" ]; then
 	SPEED=`sed -n '1p;' /sys/$DEVPATH/speed`
-	if [ $SPEED -gt 480 ]; then BUS="xhci.0"; else BUS="ehci.0"; fi;
-	echo "$SPEED $BUS $DEVPATH" >> /tmp/qusb.log
-	BUSID="busid-$BUS-`busybox basename $MDEV`"
-        echo '{ "execute": "device_add", "arguments": { "driver": "usb-host", "id": "'"$BUSID"'", "bus": "'"$BUS"'", "guest-reset": "False" } }' > /tmp/qmp/qemu.in
+	BUS=""
+	if [ $SPEED -gt 480 ]; then BUS='"bus": "xhci.0",'; fi;
+	BUSID="busid-$MAJOR-$MINOR"
+        echo '{ "execute": "device_add", "arguments": { "driver": "usb-host", "id": "'"$BUSID"'", '$BUS' "guest-reset": "False" } }' > /tmp/qmp/qemu.in
 elif [ "$ACTION" == "unbind" ]; then
-	echo "done" >> /tmp/qusb.log
-	echo '{ "execute": "device_del", "arguments": { "id": "busid-'"`busybox basename $MDEV`"'" } }' > /tmp/qmp/qemu.in
-else
-	echo "skip action" >> /tmp/qusb.log
-fi
+	echo '{ "execute": "device_del", "arguments": { "id": , "id": "'"$BUSID"'"} }' > /tmp/qmp/qemu.in
+else echo; fi
+
