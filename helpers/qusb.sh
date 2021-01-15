4bbadfc7b56ce8dad0cd08a0e7e93901641e6a13
Dmitry Fedorov <d.fedorov@tabit.pro>
2021-01-15 17:07:08 +0300

    winusb: use nec-usb-xhci by default to allow usb 3.0 win7 configuration

--- helpers/qusb.sh
+++ helpers/qusb.sh
@@ -2,10 +2,8 @@
 
 if [ "$ACTION" == "bind" ]; then
 	SPEED=`sed -n '1p;' /sys/$DEVPATH/speed`
-	BUS=""
-	if [ $SPEED -gt 480 ]; then BUS='"bus": "xhci.0",'; fi;
 	BUSID="busid-$MAJOR-$MINOR"
-        echo '{ "execute": "device_add", "arguments": { "driver": "usb-host", "id": "'"$BUSID"'", '$BUS' "guest-reset": "False" } }' > /tmp/qmp/qemu.in
+        echo '{ "execute": "device_add", "arguments": { "driver": "usb-host", "id": "'"$BUSID"'", "bus": "xhci.0", "guest-reset": "False" } }' > /tmp/qmp/qemu.in
 elif [ "$ACTION" == "unbind" ]; then
 	echo '{ "execute": "device_del", "arguments": { "id": , "id": "'"$BUSID"'"} }' > /tmp/qmp/qemu.in
 else echo; fi
